56a8fee66f8be0c9798f6d73f34fddb9d8b8f94b
Sumukha <sumukahegde007@gmail.com>
2024-08-02 23:41:25 +0530

    tic-tac-toe

--- src/components/tictactoe.tsx
+++ src/components/tictactoe.tsx
@@ -7,16 +7,24 @@ const InitializeBoard = () => {
 
 const Tictactoe = () => {
   const [board, setBoard] = useState(InitializeBoard());
+  const [text, setText] = useState("");
+  const [isPlayerXturn, setIsPlayerXTurn] = useState(true);
+
+  const handleBoardClick = (boardIndex) => {
+    isPlayerXturn ? setText("X") : setText("O");
+    setIsPlayerXTurn(!isPlayerXturn);
+  };
   return (
     <div className="board">
       <div className="header-text">
         <span>Start the Game</span>
         <button>Reset Game</button>
       </div>
-
-      {board.map((_, index) => {
-        return <button className="game-board-area">x</button>;
-      })}
+      <div className="game-board-area">
+        {board.map((_, index) => {
+          return <button onClick={() => handleBoardClick(index)}>{}</button>;
+        })}
+      </div>
     </div>
   );
 };
